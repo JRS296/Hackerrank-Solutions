234bd5132aa065a79a13bea1a024e15818174fb6
JRS296 <jonathansamuel296@gmail.com>
2022-06-28 12:16:15 +0530

    Commit #2 - Addition of 1 file(s) - 28.06.2022 @12:16

---
+++ Problem Solving/Easy/R - Circular Array Rotation.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'circularArrayRotation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY queries
+     */
+
+    public static List<Integer> circularArrayRotation(List<Integer> a, int k, List<Integer> queries) {
+    // Write your code here
+        //System.out.println("Hello");
+        List<Integer> ans = new ArrayList<Integer>();
+        int itr = a.size()-k;
+        ans = a.subList(itr, a.size());
+        for (int i=0; i<itr; i++)
+        {
+            ans.add(a.get(i));
+        }
+
+        List<Integer> finAns = new ArrayList<Integer>();
+        for(int i=0; i<queries.size(); i++)
+        {
+            finAns.add(ans.get(queries.get(i)));
+        }
+        
+        return finAns;
+    }
+
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int q = Integer.parseInt(firstMultipleInput[2]);
+
+        String[] aTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aTemp[i]);
+            a.add(aItem);
+        }
+
+        List<Integer> queries = new ArrayList<>();
+
+        for (int i = 0; i < q; i++) {
+            int queriesItem = Integer.parseInt(bufferedReader.readLine().trim());
+            queries.add(queriesItem);
+        }
+
+        List<Integer> result = Result.circularArrayRotation(a, k, queries);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
