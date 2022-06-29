57edc67f2223a430ee42284a75fa69d84adb5e8e
JRS296 <jonathansamuel296@gmail.com>
2022-06-30 01:13:40 +0530

    Commit #4 - Addition of 2 file(s) - 30.06.2022 @01:13

---
+++ Problem Solving/Easy/The Hurdle Race.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'hurdleRace' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY height
+     */
+
+    public static int hurdleRace(int k, List<Integer> height) {
+    // Write your code here
+        int gre = 0;
+        for(int i=0; i<height.size(); i++)
+        {
+            if (height.get(i)>gre)
+            {
+                gre = height.get(i);
+            }          
+        }
+        if (gre-k<0)
+        {
+            return 0;
+        }
+        return gre-k;
+    }
+
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] heightTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> height = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int heightItem = Integer.parseInt(heightTemp[i]);
+            height.add(heightItem);
+        }
+
+        int result = Result.hurdleRace(k, height);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
